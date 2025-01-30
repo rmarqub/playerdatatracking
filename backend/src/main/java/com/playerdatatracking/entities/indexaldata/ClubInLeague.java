fe6f9279a55df344dde984156102c80380f3ebd4
Larrivey <r.marques.2018@alumnos.urjc.es>
2025-01-30 17:12:04 +0100

    feat: correccion clubinleague

--- backend/src/main/java/com/playerdatatracking/entities/indexaldata/ClubInLeague.java
+++ backend/src/main/java/com/playerdatatracking/entities/indexaldata/ClubInLeague.java
@@ -3,6 +3,9 @@ package com.playerdatatracking.entities.indexaldata;
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
@@ -12,16 +15,13 @@ import jakarta.persistence.Table;
 @Table(name = "club_in_league")
 public class ClubInLeague {
 
-    @EmbeddedId
-    private ClubInLeagueId id;
-
-    @ManyToOne
-    @MapsId("clubId")
-    @JoinColumn(name = "club", referencedColumnName = "id", nullable = false)
-    private Club club;
-
-    @ManyToOne
-    @MapsId("torneoId")
-    @JoinColumn(name = "torneo", referencedColumnName = "id", nullable = false)
-    private Torneo torneo;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+    @Column(name = "club")
+	private Long clubId;
+    
+    @Column(name = "torneo")
+    private Long torneoId;
 }
