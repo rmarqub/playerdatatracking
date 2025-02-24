6b1f0450636ce009436dc78053a083c7333cff86
Larrivey <r.marques.2018@alumnos.urjc.es>
2025-02-24 17:07:09 +0100

    feat: search functional, but not perfect

--- frontend/playerdatatrackingfront/playerdatatrackerfront/src/app/services/player-service.service.ts
+++ frontend/playerdatatrackingfront/playerdatatrackerfront/src/app/services/player-service.service.ts
@@ -1,43 +1,63 @@
 import { Player } from './../entitites/player';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { ManualTrackedPlayer } from 'src/app/entitites/manual-tracker-player';
 import { catchError, map } from 'rxjs/operators';
+
+interface GenericResponse<T> {
+  code: number;
+  description: string;
+  entity?: T;
+  entityList?: T[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
-  private apiUrl = 'http://localhost:8080/player';
+  private apiUrlManualP = 'http://localhost:8080/player';
+  private apiUrlIndexalP = 'http://localhost:8080/search';
 
   constructor(private http: HttpClient) { }
 
   getPlayer(id: number): Observable<ManualTrackedPlayer | null> {
-    const url = `${this.apiUrl}/${id}`;  // Concatenamos el ID en la URL
+    const url = `${this.apiUrlManualP}/${id}`;
 
-    return this.http.get<any>(url)
-      .pipe(
-        map(response => {
-          if (response.code === 0) {
-            return response.entity as ManualTrackedPlayer;
-          } else {
-            console.error(response.description);
-            return null;
-          }
-        }),
-        catchError(error => {
-          console.error('Error en la llamada GET:', error);
-          return of(null);  // Devuelve un observable con valor null en caso de error
-        })
-      );
+    return this.http.get<GenericResponse<ManualTrackedPlayer>>(url).pipe(
+      map(response => {
+        if (response.code === 0) {
+          return response.entity as ManualTrackedPlayer;
+        } else {
+          console.error(response.description);
+          return null;
+        }
+      }),
+      catchError(error => {
+        console.error('Error en la llamada GET:', error);
+        return of(null);
+      })
+    );
   }
 
   searchPlayers(playerName?: string, teamName?: string): Observable<Player[]> {
     let params: any = {};
-    if (playerName) params.playerName = playerName;
-    if (teamName) params.teamName = teamName;
+    if (playerName) params.player = playerName;
+    if (teamName) params.team = teamName;
 
-    return this.http.get<Player[]>(this.apiUrl, { params });
+    return this.http.get<GenericResponse<Player>>(this.apiUrlIndexalP, { params }).pipe(
+      map(response => {
+        if (response.code === 0) {
+          return response.entityList || [];
+        } else {
+          console.error(response.description);
+          return [];
+        }
+      }),
+      catchError(error => {
+        console.error('Error en la búsqueda de jugadores:', error);
+        return of([]);
+      })
+    );
   }
 }
